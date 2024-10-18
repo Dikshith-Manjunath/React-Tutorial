7da6659a8ddd7f078fdbcbe0eee38fb55ee2ecf1
Dikshith <dikshithrrddy@gmail.com>
2024-10-19 00:15:54 +0530

    Updates

--- src/components/Admin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Admin() {
-  return (
-    <div className='my-3 mx-3'>
-      Admin Details:
-    </div>
-  )
-}
--- src/components/FeaturedProducts.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function FeaturedProducts() {
-  return (
-    <div className='mx-3'>
-      <h3>List of Featured Products:</h3>
-    </div>
-  )
-}
